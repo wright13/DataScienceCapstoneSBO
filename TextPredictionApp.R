999aebfa1260d9328aff0eee5bdbcd0d385ea03e
Wright <sewright@nps.gov>
2018-03-13 12:42:32 -0700

    buttons work, added instructions, fixed text input width issue

--- TextPredictionApp.R
+++ TextPredictionApp.R
@@ -2,6 +2,14 @@ library(shiny)
 source("StupidBackoff.R")
 
 ui <- fluidPage(
+    tags$head(
+        tags$style(HTML("
+            div.form-group.shiny-input-container{
+                width: 100%;
+                height: 100%;
+            }     
+        "))
+    ),
     tags$script('
       Shiny.addCustomMessageHandler("refocus",
             function(e_id) {
@@ -11,16 +19,29 @@ ui <- fluidPage(
     fluidRow(
         column(width = 12,
                h1("Word Predictor")
-               )
+        )
     ),
     sidebarLayout(
-        sidebarPanel(),
+        sidebarPanel(
+            h3("About this app"),
+            p("If you've ever tried to write something, you know how hard it can be to put your thoughts into text. 
+              Sometimes it seems impossible to figure out what your next word should be. That's where WordPredictor comes in. 
+              WordPredictor analyzes millions of lines of text sourced from blogs, news, and Twitter to suggest your next word. With the eloquence of Twitter and the 
+              blogosphere at your disposal, you'll never suffer from writer's block again."),
+            h3("Instructions"),
+            tags$ul(
+                tags$li("Begin typing into the text box to the right"),
+                tags$li("When you pause for thought, up to three word suggestions will appear to the right of the text box"),
+                tags$li("Click on a suggestion to insert it as the next word"),
+                tags$li("Need more space to type? Click and drag the bottom right corner of the text box to resize it.")
+            )
+        ),
         mainPanel(
             fluidRow(
                 column(width = 12, h4("Enter some text!"))
             ),
             fluidRow(
-                column(width = 8, textAreaInput(inputId = "textIn", label = NULL, width = '100%', height = '50%', resize = "none")),
+                column(width = 8, textAreaInput(inputId = "textIn", label = NULL, width = '100%', resize = "vertical")),
                 column(width = 4, uiOutput("predButton1", inline = TRUE), 
                        uiOutput("predButton2", inline = TRUE),
                        uiOutput("predButton3", inline = TRUE)
