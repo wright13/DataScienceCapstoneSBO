fce0e1379b18f511d939b8c2e84afa8d920963fe
Wright <sewright@nps.gov>
2018-03-13 19:00:38 -0700

    Finished all but last slide, removde cross-entropy from test

--- Slides.md
+++ Slides.md
@@ -19,9 +19,14 @@ FinishMyThought uses a simple but effective word prediction algorithm called Stu
 The collection of phrases is searched for the last four words of your input into FinishMyThought, and the Stupid Backoff algorithm returns the word(s) most likely to follow. If those four words are not found, the last three words of input are used instead, and so on until a prediction is found. If even the last word of input doesn't appear in the collection of known phrases, the algorithm still does the best it can by predicting words that most frequently appear after uncommon words (these were consolidated under a single code in the phrase collection).
 
 
-Performance
+Performance by the numbers
 ========================================================
-
+- **5000**:
+    + The approximate number of phrases used to test the Stupid Backoff algorithm
+- **17%**:
+    + Single-word accuracy (the percentage of the time that the word predicted to be most likely was in fact correct)
+- **27%**:
+    + Three-word accuracy (the percentage of the time that one of the predicted words was correct)
 
 
 Shiny App
--- TestStupidBackoff.R
+++ TestStupidBackoff.R
@@ -6,10 +6,10 @@ library(readr)
 library(R.utils)
 library(readtext)
 library(sqldf)
-source("StupidBackoff.R")
+source(paste0(getwd(),"/StupidBackoff/StupidBackoff.R"))
 
 # Read texts into invididual corpus. readtext() works for twitter and blogs, and read_file() works for news.
-test.path <- paste0(getwd(), "/test.txt")
+test.path <- paste0(getwd(), "/StupidBackoff/test.txt")
 corp <- corpus(readtext(test.path))
 tokns <- tokens(corp, what = "word", remove_numbers = TRUE, remove_punct = TRUE, remove_symbols = TRUE, remove_url = TRUE, remove_twitter = TRUE, ngrams = 5) %>%
     tokens_tolower() %>%
@@ -21,35 +21,28 @@ tokns <- tokns[, list(prefix = sub("_[^_]+$", "", n.gram), word = sub("^([^_]+_)
 set.seed(1302018)
 sample.tokens <- tokns[rbinom(nrow(tokns), 1, 5000/nrow(tokns)) == 1]
 
-fun <- function(x) {
+topPred <- function(x) {
     r <- SBO(x)
     r <- setorder(r, -prob)
     w <- r[1, c("word", "prob")]
     return(w)
 }
 
-fun2 <- function(x) {
+top3Pred <- function(x) {
     r <- SBO(x)
-    r <- r[,1:2]
+    r <- r[, c("word", "prob")]
     return(r)
 }
 
-xentropy <- 0
 matches.top3 <- 0
 for (i in 1:nrow(sample.tokens)) {
-    pred <- fun(sample.tokens[i, prefix])
-    pred.top3 <- fun2(sample.tokens[i, prefix])
+    pred <- topPred(sample.tokens[i, prefix])
+    pred.top3 <- top3Pred(sample.tokens[i, prefix])
     
-    if (any(is.na(pred))) pred <- c("UNK", "0") 
     result <- sample.tokens[i, prediction := pred[[1]]]
-    if (pred[[2]] > 0) xentropy <- xentropy + log(pred[[2]])
     
     if (nrow(pred.top3) > 0) matches.top3 <- matches.top3 + any(pred.top3$word == sample.tokens[i, word])
 }
 
-avg.x.entropy <- -xentropy/nrow(sample.tokens)
 accuracy <- nrow(sample.tokens[word == prediction]) / nrow(sample.tokens)
 accuracy.top3 <- matches.top3 / nrow(sample.tokens)
-
-
-# test <- sample.tokens[, list(prefix, word, prediction = sapply(prefix, fun)[1])]
